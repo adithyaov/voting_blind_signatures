c6a2ed03e5cdcb7b512e0d92b4177eb646e7264d
Adithya Kumar <111501017@smail.iitpkd.ac.in>
2018-04-08 18:30:57 +0530

    Added thread safety, and votings dict

--- src/web_app/server.py
+++ src/web_app/server.py
@@ -2,10 +2,20 @@ from bottle import run, static_file, post, get, request, template, response
 from ..utils import base_path, read_ballot, verify_voter
 from os.path import join
 from json import dumps
+import threading
 
 response.content_type = 'application/json'
 
-create_ballots(['b1', 'b2', 'b3'])
+ballots = ['b1', 'b2', 'b3']
+
+create_ballots(ballots)
+
+votings_lock = threading.Lock()
+votings = {}
+
+for b in ballots:
+	votings[b] = []
+
 
 @get('/public-key/<ballot_name>')
 def download_public_key(ballot_name):
@@ -15,7 +25,12 @@ def download_public_key(ballot_name):
 def sign_message(ballot_name):
 	try:
 		data = request.json
-		if verify_voter(data['token']):
+		id, email, is_valid = verify_voter(data['token'])
+		if email in votings[ballot_name]:
+			return dumps({'code': 409, 'error': 'Already casted vote.'})
+		if is_valid:
+			with votings_lock:
+				votings[ballot_name].append(email)
 			A = read_ballot(ballot_name)
 			return dumps({'code': 200, 'signed_msg': A.sign(data['msg'])})
 		return dumps({'code': 400, 'error': 'Not a valid voter.'})
