adcc9417284e33f0efbf94ac35ac4ac18120c126
Adithya Kumar <111501017@smail.iitpkd.ac.in>
2018-04-08 18:09:49 +0530

    Added methods to downlod public keys, saved ballots

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,8 @@
 import cPickle as pickle
-from resource import *
+from resource import Authority
 from os.path import join
+
+base_path = '/home/adithya/Prog/networking-project/src/'
 
 def save_object(obj, file_path):
 	with open(file_path, 'wb') as file:
@@ -9,20 +11,23 @@ def save_object(obj, file_path):
 
 def create_ballot(ballot_name):
 	A = Authority()
-	save_object(A.priv_key, join('./keys/private', ballot_name))
-	save_object(A.pub_key, join('./keys/pub_key', ballot_name))
+	save_object(A, join(base_path, 'data/authority-objects/', ballot_name))
+	save_object(A.pub_key, join(base_path, 'data/public-keys/', ballot_name))
 	return A.priv_key, A.pub_key
 
 
+def read_ballot(ballot_name):
+	with open(join(base_path, 'data/authority-objects/', ballot_name), 'rb') as input:
+	    return pickle.load(input)
 
 
+def verify_voter(token):
+	return False
 
 
-
-
-
-
-
+def create_ballots(ballots)
+	for b in ballots:
+		create_ballot(b)
 
 
 
--- src/web_app/server.py
+++ src/web_app/server.py
@@ -1,13 +1,27 @@
-from bottle import route, run, template
-from ...utils import create_ballot
+from bottle import run, static_file, post, get, request, template, response
+from ..utils import base_path, read_ballot, verify_voter
+from os.path import join
+from json import dumps
 
-ballots = ['b1', 'b2', 'b3']
-for b in ballots:
-	create_ballot(b)
+response.content_type = 'application/json'
 
-@route('/hello/<name>')
-def index(name):
-    return template('<b>Hello {{name}}</b>!', name=name)
+create_ballots(['b1', 'b2', 'b3'])
+
+@get('/public-key/<ballot_name>')
+def download_public_key(ballot_name):
+    return static_file(ballot_name, root=join(base_path, 'data/public-keys'), download=ballot_name)
+
+@post('/sign-blind-msg/<ballot_name>')
+def sign_message(ballot_name):
+	try:
+		data = request.json
+		if verify_voter(data['token']):
+			A = read_ballot(ballot_name)
+			return dumps({'code': 200, 'signed_msg': A.sign(data['msg'])})
+		return dumps({'code': 400, 'error': 'Not a valid voter.'})
+	except:
+		return dumps({'code': 500, 'error': 'Improper data format.'})
+
 
 run(host='localhost', port=8080)
 
