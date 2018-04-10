7a7bc80d2812711edc834edb3148517b70a2eb41
Adithya Kumar <111501017@smail.iitpkd.ac.in>
2018-04-11 04:40:43 +0530

    User almost done, remade

--- src/utils.py
+++ src/utils.py
@@ -7,72 +7,101 @@ import datetime
 
 base_path = '/home/adithya/Prog/networking-project/src/'
 
+
 def log(type, text):
-	print '{} : [{}] {}'.format(datetime.datetime.now(), type.upper(), text)
+    print '{} : [{}] {}'.format(datetime.datetime.now(), type.upper(), text)
+
 
 def save_object(obj, file_path):
-	with open(file_path, 'wb') as file:
-		pickle.dump(obj, file, pickle.HIGHEST_PROTOCOL)
+    with open(file_path, 'wb') as file:
+        pickle.dump(obj, file, pickle.HIGHEST_PROTOCOL)
+
 
 def open_object(file_path):
-	with open(file_path, 'rb') as file:
-		return pickle.load(file)
+    with open(file_path, 'rb') as file:
+        return pickle.load(file)
 
 
 def create_ballot(ballot_name):
-	A = Authority()
-	save_object(A, join(base_path, 'data/authority-objects/', ballot_name))
-	save_object(A.pub_key, join(base_path, 'data/public-keys/', ballot_name))
-	return A.priv_key, A.pub_key
+    A = Authority()
+    save_object(A, join(base_path, 'data/authority-objects/', ballot_name))
+    save_object(A.pub_key, join(base_path, 'data/public-keys/', ballot_name))
+    return A.priv_key, A.pub_key
 
 
 def read_ballot(ballot_name):
-	return open_object(join(base_path, 'data/authority-objects/', ballot_name))
+    return open_object(join(base_path, 'data/authority-objects/', ballot_name))
+
 
 def verify_voter(token):
-	test_users = {
-		'email1': md5('password1').hexdigest(),
-		'email2': md5('password2').hexdigest(),
-		'email3': md5('password3').hexdigest(),
-		'email4': md5('password4').hexdigest()
-	}
-	try:
-		if test_users[token['email']] == token['password_hash']:
-			log('info', 'Successful user Auth.')
-			return token['email'], True
-		log('info', 'Failed user Auth.')
-		return token['email'], False
-	except Exception as e:
-		log('exception verify voter', e)
-		return 0, False
+    test_users = {
+        'email1': md5('password1').hexdigest(),
+        'email2': md5('password2').hexdigest(),
+        'email3': md5('password3').hexdigest(),
+        'email4': md5('password4').hexdigest()
+    }
+    try:
+        if test_users[token['email']] == token['password_hash']:
+            log('info', 'Successful user Auth.')
+            return token['email'], True
+
+        log('info', 'Failed user Auth.')
+        return token['email'], False
+
+    except Exception as e:
+        log('exception verify voter', e)
+        return 0, False
 
 
+true_parties = {
+    'b1': ['b1_p0', 'b1_p1', 'b1_p2', 'b1_p3'],
+    'b2': ['b2_p0', 'b2_p1', 'b2_p2', 'b2_p3'],
+    'b3': ['b3_p0', 'b3_p1', 'b3_p2', 'b3_p3']
+}
 
 def verify_vote(vote, prev_votes, ballot_name):
-	participants_in_ballot = {
-		'b1': ['b1_p0', 'b1_p1', 'b1_p2', 'b1_p3'],
-		'b2': ['b2_p0', 'b2_p1', 'b2_p2', 'b2_p3'],
-		'b3': ['b3_p0', 'b3_p1', 'b3_p2', 'b3_p3']
-	}
-	try:
-		timestamp_eq = filter(lambda x: x['msg']['timestamp'] == vote['msg']['timestamp'], prev_votes)
-		if len(timestamp_eq) > 0:
-			log('info', 'Vote clash, vote: {}, votes: {}'.format(str(vote), str(timestamp_eq)))
-			return False
-		if vote['msg']['participant'] not in participants_in_ballot[ballot_name]:
-			log('info', 'Invalid participant, participant: {}, valid_participants: {}'
-				.format(str(vote['participant']), str(participants_in_ballot[ballot_name])))
-			return False
 
-		A = read_ballot(ballot_name)
-		print '---------------------------',A.verify_msg(str(vote['msg']), vote['msg_signature'])
-		if not A.verify_msg(str(vote['msg']), vote['msg_signature']):
-			log('info', 'Invalid signature, vote: {}'.format(str(vote)))
-			return False
+    try:
+        timestamp_eq = filter(lambda x: x['msg']['timestamp'] ==
+                              vote['msg']['timestamp'], prev_votes)
 
-		log('info', 'Successful vote Auth, vote: {}'.format(str(vote)))
-		return True
-	except Exception as e:
-		log('exception verify vote', e)
-		return 0, False
+        if len(timestamp_eq) > 0:
+            log('info', 'Vote clash, vote: {}, votes: {}'
+                .format(str(vote['msg']),
+                        str(map(lambda x: x['msg'], timestamp_eq))))
+            return False
+
+        if vote['msg']['party'] not in true_parties[ballot_name]:
+            log('info', 'Invalid party, party: {}, valid_partys: {}'
+                .format(str(vote['party']), str(true_parties[ballot_name])))
+            return False
+
+        A = read_ballot(ballot_name)
+
+        if not A.verify_msg(str(vote['msg']), vote['msg_signature']):
+            log('info', 'Invalid signature, vote: {}'.format(str(vote['msg'])))
+            return False
+
+        log('info', 'Successful vote Auth, vote: {}'.format(str(vote['msg'])))
+        return True
+    except Exception as e:
+        log('exception verify vote', e)
+        return 0, False
+
+
+
+def read_votes(ballot_name):
+	votes = open_object(join(base_path, 'data/votes/', ballot_name))
+	vote_counts = {}
+
+	for p in true_parties[ballot_name]:
+		vote_counts[p] = 0
+
+	for x in votes:
+		vote_counts[x['msg']['party']] += 1
+
+	return vote_counts
+
+
+
 
--- src/web_app/server.py
+++ src/web_app/server.py
@@ -1,9 +1,10 @@
 from bottle import run, static_file, post, get, request, template, response
 from ..utils import (base_path, read_ballot, verify_voter, create_ballot,
-                     verify_vote, save_object, log, open_object)
+                     verify_vote, save_object, log, open_object, read_votes)
 from os.path import join
-from json import dumps
+from json import dumps, loads
 import threading
+from ast import literal_eval
 
 response.content_type = 'application/json'
 
@@ -30,23 +31,48 @@ def download_public_key(ballot_name):
                        download=ballot_name)
 
 
+@get('/download-votes/<ballot_name>')
+def download_votes(ballot_name):
+    log('info', 'Votes of {} requested.'.format(ballot_name))
+    return static_file(ballot_name,
+                       root=join(base_path, 'data/votes'),
+                       download=ballot_name)
+
+@get('/votes/<ballot_name>')
+def show_votes(ballot_name):
+    return read_votes(ballot_name)
+
+
 @post('/sign-blind-msg/<ballot_name>')
 def sign_message(ballot_name):
     try:
-        data = request.json
-        email, is_valid = verify_voter(data['token'])
+        email = request.forms.get('email')
+        password_hash = request.forms.get('password_hash')
+        token = {
+            'email': email,
+            'password_hash': password_hash
+        }
+        msg = request.forms.get('msg')
+
+        email, is_valid = verify_voter(token)
+
         if email in voters[ballot_name]:
             log('info', '{} tried to vote again.'.format(email))
             return dumps({'code': 409, 'error': 'Already casted vote.'})
+
         if is_valid:
             A = read_ballot(ballot_name)
-            signed_msg = A.sign(data['msg'])
+            msg_signature = A.sign(msg)
+
             with voter_locks[ballot_name]:
                 voters[ballot_name].append(email)
+
             log('info', '{} successfully taken a signed vote.'.format(email))
-            return dumps({'code': 200, 'signed_msg': signed_msg})
+            return dumps({'code': 200, 'msg_signature': msg_signature})
+
         log('info', '{} is an invalid voter.'.format(email))
         return dumps({'code': 400, 'error': 'Not a valid voter.'})
+
     except Exception as e:
         log('exception sign', e)
         return dumps({'code': 500, 'error': e})
@@ -55,17 +81,28 @@ def sign_message(ballot_name):
 @post('/dump-vote/<ballot_name>')
 def dump_vote(ballot_name):
     try:
-        data = request.json
-        while ballot_locks[ballot_name]:
+        msg = literal_eval(request.forms.get('msg'))
+        msg_signature = long(request.forms.get('msg_signature'))
+
+        vote = {
+            'msg': msg,
+            'msg_signature': msg_signature
+        }
+
+        with ballot_locks[ballot_name]:
             path = join(base_path, 'data/votes/', ballot_name)
             prev_votes = open_object(path)
-            if not verify_vote(data['vote'], prev_votes, ballot_name):
-                log('info', 'Invalid vote, Vote: {}'.format(data['vote']))
+
+            if not verify_vote(vote, prev_votes, ballot_name):
+                log('info', 'Invalid vote, vote: {}'.format(vote['msg']))
                 return dumps({'code': 408, 'error': 'Invalid vote.'})
-            prev_votes.append(data['vote'])
+
+            prev_votes.append(vote)
             save_object(prev_votes, path)
-        log('info', 'Vote dumped, Vote: {}'.format(data['vote']))
+
+        log('info', 'Vote dumped, Vote: {}'.format(vote['msg']))
         return dumps({'code': 200, 'msg': 'Successfully dumped vote.'})
+
     except Exception as e:
         log('exception dump', e)
         return dumps({'code': 500, 'error': e})
